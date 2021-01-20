204f03d7095f35694a63f39f2bf50c5db8a50c65
Marko Djokic <marko1319@its.edu.rs>
2021-01-20 17:16:00 +0100

    Add project files.

---
+++ Delegati2/Knjiga.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegati2
+{
+    public class Knjiga    // Opis k u listi k:
+    {
+        public string Naslov; // Naslov knjige.
+        public string Autor; // Autor knjige.
+        public decimal Cena; // Cena knjige.
+        public bool Tvrdpovez; // Da li je tvrd povez?
+
+        public Knjiga(string naslov, string autor, decimal cena, bool tvrdPovez)
+        {
+            Naslov = naslov;
+            Autor = autor;
+            Cena = cena;
+            Tvrdpovez = tvrdPovez;
+        }
+    }
+}
